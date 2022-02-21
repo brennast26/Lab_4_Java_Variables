d8d8c7ad68dc89e1055cecd70c13d42ae78a43f6
Brennast26 <brennast@mail.uc.edu>
2022-02-21 12:37:52 -0500

    Part 1 of lab 4.

---
+++ src/com/company/Main.java
@@ -0,0 +1,65 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args)
+    {
+	int intOperandA = 15;
+    int intOperandB = 13;
+    int intSum = 24;
+    int intProduct = 29;
+    int intDifference = 33;
+    int intQuotient = 37;
+    int intModulo = 42;
+
+    double doubleOperandA = 3.50;
+    double doubleOperandB = 7.30;
+    double doubleSum = 12.40;
+    double doubleProduct = 17.10;
+    double doubleDifference = 20.40;
+    double doubleQuotient = 24.70;
+    double doubleModulo=28.20;
+
+    intSum = intOperandA + intOperandB;
+    System.out.println("The sum using ints of "+intOperandA+" "+intOperandB+" is "+intSum);
+    intProduct = intOperandA * intOperandB;
+    System.out.println("The Product using ints of "+intOperandA+" "+intOperandB+" is "+intProduct);
+    intDifference = intOperandA - intOperandB;
+    System.out.println("The difference using ints of "+intOperandA+" "+intOperandB+" is "+intDifference);
+    intQuotient = intOperandA / intOperandB;
+    System.out.println("The quotient using ints of "+intOperandA+" "+intOperandB+" is "+intQuotient);
+    intModulo = intOperandA % intOperandB;
+    System.out.println("The modulo using ints of "+intOperandA+" "+intOperandB+" is "+intModulo);
+
+    doubleSum = doubleOperandA + doubleOperandB;
+    System.out.println("The sum using double of "+doubleOperandA+" "+doubleOperandB+" is "+doubleSum);
+    doubleProduct = doubleOperandA * doubleOperandB;
+    System.out.println("The Product using ints of "+doubleOperandA+" "+doubleOperandB+" is "+doubleProduct);
+    doubleDifference = doubleOperandA - doubleOperandB;
+    System.out.println("The difference using ints of "+doubleOperandA+" "+doubleOperandB+" is "+doubleDifference);
+    doubleQuotient = doubleOperandA / doubleOperandB;
+    System.out.println("The quotient using ints of "+doubleOperandA+" "+doubleOperandB+" is "+doubleQuotient);
+    doubleModulo = doubleOperandA % doubleOperandB;
+    System.out.println("The modulo using ints of "+doubleOperandA+" "+doubleOperandB+" is "+doubleModulo);
+
+
+    double mysalary = 12.50;
+    System.out.println("My current salary is $"+mysalary);
+    int mysiblings = 1;
+    System.out.println("I have "+mysiblings+ " sibling.");
+    String isitwindy = "No";
+    System.out.println("Is it windy today? "+isitwindy);
+    double priceofbread = 2.50;
+    System.out.println("The current price of bread is $"+priceofbread);
+    String favoritecolor = "Blue";
+    System.out.println("My favorite color is "+favoritecolor);
+    int ringsize = 9;
+    System.out.println("My ring size is "+ringsize);
+    int birthyear = 2002;
+    System.out.println("My birth year is "+birthyear);
+    String fullname = "Sean Thomas Brennan";
+    System.out.println("My full name is "+fullname);
+
+
+    }
+}
